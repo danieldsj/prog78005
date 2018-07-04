3b081644e80fddc9867b2a98b7608c37da099093
Daniel de Sao Jose <danieldsj@gmail.com>
2018-07-03 21:10:19 -0400

    First commit!

---
+++ src/DanielExperiment.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+
+
+public class DanielExperiment extends Application {
+
+    @Override
+    public void start (Stage myStage) {
+
+        // An application automatically gets a single default Stage object.
+        // A Stage object can have one scene at a time and is set with the setScene method.
+        // A Scene object has only one pane or control set with the constructor.
+        // A Scene cannot have nodes such as images and shapes.
+        // Images and shapes must be added to a pane before being added to a scene.
+        // Panes can be nested.
+
+        // Create an image and an ImageView object (Node).
+        Image image = new Image("file:///home/daniel/Documents/src/prog78005/src/cow.jpg", 600,
+                600, false, false);
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+
+        // Add the ImageView object (Node) to the Pane.
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(imageView);
+
+        // Create a Scene object with the Pane object.
+        Scene scene = new Scene(stackPane, 600, 600);
+        imageView.fitHeightProperty().bind(scene.heightProperty());
+        imageView.fitWidthProperty().bind(scene.widthProperty());
+
+        // Load the scene to the stage
+        myStage.setTitle("This is a mooing application!");
+        myStage.setScene(scene);
+        myStage.show();
+    }
+
+
+    public static void main (String[] args) {
+        launch();
+    }
+
+}
