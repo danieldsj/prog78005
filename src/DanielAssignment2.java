416fe8c0e8ee369d595405640d477b90639bc836
Daniel de Sao Jose <danieldsj@gmail.com>
2018-07-13 00:13:15 -0400

    Removed unnecessary import statements.

--- src/DanielAssignment2.java
+++ src/DanielAssignment2.java
@@ -19,8 +19,6 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import java.util.Random;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
 import java.util.ArrayList;
 
 
