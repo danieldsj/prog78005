1d97dfc5b2c58ab7fcd56fe5e5f9e554f2dcd3c5
Daniel de Sao Jose <danieldsj@gmail.com>
2018-07-26 07:38:37 -0400

    Looked up assignment 5 and updated comments.

--- src/DanielAssignment5.java
+++ src/DanielAssignment5.java
@@ -1,8 +1,24 @@
 // Generic Classes
 // Exercise 19.9
 
+import java.util.ArrayList;
+
+/**
+ * Generic Classes
+ * Written by Daniel de Sao Jose
+ *
+ * (Sort ArrayList ) Write the following method that sorts an ArrayList :
+ * public static <E extends Comparable<E>>
+ * void sort(ArrayList<E> list)
+ */
+
 public class DanielAssignment5 {
     public static void main (String[] args) {
+
         System.out.println("Assignment 5");
+    }
+
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+
     }
 }
