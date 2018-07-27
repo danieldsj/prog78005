4624cf369e29cf6da71ee6abcfbd20f79a1b5920
Daniel de Sao Jose <danieldsj@gmail.com>
2018-07-27 15:21:47 -0400

    Completed assignment 5, I think.

--- src/DanielAssignment5.java
+++ src/DanielAssignment5.java
@@ -2,6 +2,8 @@
 // Exercise 19.9
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 /**
  * Generic Classes
@@ -13,12 +15,51 @@ import java.util.ArrayList;
  */
 
 public class DanielAssignment5 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        System.out.println("Assignment 5");
+        Random random = new Random();
+
+        // Create an ArrayList of Integers. Integers are comparable.
+        ArrayList<Integer> myIntegerList = new ArrayList<>();
+        for(int i = 0; i < 6; i++) {
+            myIntegerList.add(random.nextInt(10));
+        }
+
+        // Exercise using sort with the ArrayList of Integer objects.
+        System.out.println("An ArrayList of Integer objects contain the following elements:");
+        for (Object o : myIntegerList) {
+            System.out.println(" " + o);
+        }
+        System.out.println("After using the sort method, the ArrayList of Integer objects contain the " +
+                "following elements:");
+        sort(myIntegerList);
+        for (Object o : myIntegerList) {
+            System.out.println(" " + o);
+        }
+        System.out.println();
+
+        // Create an ArrayList of Doubles. Doubles are comparable.
+        ArrayList<Double> myDoubleList = new ArrayList<>();
+        for(int i = 0; i < 6; i++) {
+            myDoubleList.add(random.nextDouble());
+        }
+
+        System.out.println("An ArrayList of Double objects contain the following elements:");
+        for (Object o : myDoubleList) {
+            System.out.println(" " + o);
+        }
+        sort(myDoubleList);
+        System.out.println("After using the sort method, the ArrayList of Double objects contain the " +
+                "following elements:");
+        for (Object o : myDoubleList) {
+            System.out.println(" " + o);
+        }
+        System.out.println();
     }
 
     public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
-
+        Collections.sort(list);
     }
+
 }
+
